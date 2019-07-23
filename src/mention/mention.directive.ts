8a283de4e9afb2c10e0a74b805b6006ae757c885
Andrzej Grzelak <andrzej.grzelak@bcf-software.pl>
2019-07-23 12:12:55 +0200

    Fix for android version

--- src/mention/mention.directive.ts
+++ src/mention/mention.directive.ts
@@ -254,7 +254,7 @@ export class MentionDirective implements OnChanges {
         } else {
           console.debug('mention.directive.ts keyHandler, val:', val,event.keyCode);
           let mention = val.substring(this.startPos + 1, pos);
-          if (event.keyCode !== KEY_BACKSPACE) {
+          if (event.keyCode !== KEY_BACKSPACE && event.keyCode !== KEY_BUFFERED) {
             mention += charPressed;
           }
           this.searchString = mention;
